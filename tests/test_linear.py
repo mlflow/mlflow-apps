7431675cb7c230d3e6794595a59aa5c07b80317e
Juntai Zheng <juntai.zheng@databricks.com>
2018-07-27 14:32:21 -0700

    added debugging prints

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+print(sys.path)
+print(sys.executable)
 import os
 import numpy
 import pandas
